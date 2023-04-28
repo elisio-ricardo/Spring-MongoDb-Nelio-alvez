d34f5fc79c8108774b6047176145e3e5cdb1865b
Elisio Ricardo <elisio.silva@sysmap.com>
2023-04-28 15:52:50 -0300

    criando o repository e fazendo a chamado ao banco

---
+++ src/main/java/com/MongoDB/workshop/services/UserService.java
@@ -0,0 +1,19 @@
+package com.MongoDB.workshop.services;
+
+import com.MongoDB.workshop.domain.User;
+import com.MongoDB.workshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
